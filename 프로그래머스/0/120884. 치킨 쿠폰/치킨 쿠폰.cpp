37cb3df82c8edc107eb3af501ad8334ede477496
Hwangsangjin <59340471+Hwangsangjin@users.noreply.github.com>
2026-05-23 18:32:36 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.01 ms, Memory: 3.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.cpp
@@ -0,0 +1,14 @@
+int solution(int chicken)
+{
+    int answer = 0;
+    
+    while (chicken >= 10)
+    {
+        int service = chicken / 10;
+        answer += service;
+        
+        chicken = service + (chicken % 10);
+    }
+    
+    return answer;
+}
