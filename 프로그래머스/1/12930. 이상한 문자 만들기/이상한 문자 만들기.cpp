880fc03536a323861725f61a0eddbce0e4e0a415
Hwangsangjin <59340471+Hwangsangjin@users.noreply.github.com>
2026-07-13 23:38:41 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.01 ms, Memory: 4.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.cpp
@@ -0,0 +1,28 @@
+#include <cctype>
+#include <string>
+
+using namespace std;
+
+string solution(string s)
+{
+    int count = 0;
+    
+    for (int i = 0; i < s.length(); ++i)
+    {
+        if (s[i] == ' ')
+        {
+            count = 0;
+        }
+        else
+        {
+            if (count % 2 == 0)
+                s[i] = toupper(s[i]);
+            else
+                s[i] = tolower(s[i]);
+
+            ++count;
+        }
+    }
+    
+    return s;
+}
