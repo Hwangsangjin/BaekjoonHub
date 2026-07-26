d0d4f85d6527af8c3e882d5f8602ec28ad1f4f27
Hwangsangjin <59340471+Hwangsangjin@users.noreply.github.com>
2026-07-26 21:57:32 +0900

    [level 2] Title: 올바른 괄호, Time: 0.10 ms, Memory: 4.45 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.cpp
@@ -0,0 +1,21 @@
+#include <string>
+
+using namespace std;
+
+bool solution(string s)
+{
+    int count = 0;
+    
+    for (char c : s)
+    {
+        if (c == '(')
+            ++count;
+        else
+            --count;
+        
+        if (count < 0)
+            return false;
+    }
+    
+    return count == 0;
+}
