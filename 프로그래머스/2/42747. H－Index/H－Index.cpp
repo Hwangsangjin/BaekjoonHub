da94d7b710c90911311cdc29a7753a20e467b338
Hwangsangjin <59340471+Hwangsangjin@users.noreply.github.com>
2026-08-28 12:33:51 +0900

    [level 2] Title: H-Index, Time: 0.04 ms, Memory: 4.95 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.cpp
@@ -0,0 +1,17 @@
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> citations)
+{
+    sort(citations.rbegin(), citations.rend());
+
+    for (int i = 0; i < citations.size(); ++i)
+    {
+        if (citations[i] < i + 1)
+            return i;
+    }
+
+    return citations.size();
+}
