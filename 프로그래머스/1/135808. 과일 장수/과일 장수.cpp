e0252fabdfbcc80de75deac37c178b4864b2f0b5
Hwangsangjin <59340471+Hwangsangjin@users.noreply.github.com>
2026-08-30 23:15:09 +0900

    [level 1] Title: 과일 장수, Time: 18.79 ms, Memory: 35 MB -BaekjoonHub

---
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.cpp
@@ -0,0 +1,16 @@
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int solution(int k, int m, vector<int> score)
+{
+    sort(score.begin(), score.end(), greater<int>());
+    
+    int answer = 0;
+    
+    for (int i = m - 1; i < score.size(); i += m)
+        answer += score[i] * m;
+    
+    return answer;
+}
